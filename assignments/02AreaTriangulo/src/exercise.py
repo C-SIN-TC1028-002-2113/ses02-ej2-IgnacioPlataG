2b56129ce4af8ba65c22c0c2a21e154dc1248c56
IgnacioPlataG <A01740693@itesm.mx>
2021-08-13 14:48:59 +0000

    Ejericicio 1

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -4,6 +4,6 @@ def main():
     b = float(input("Dame la base: "))
     h = float(input("dame la altura: "))
     a = (b*h)/2
-    print("El area es: ",a)
+    print("El area es:  ",a)
 if __name__ == '__main__':
     main()
